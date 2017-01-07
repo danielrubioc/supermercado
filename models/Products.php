f0dac8ffaa2e6d0db1b6c03f5d160059432cc62a
Julians Iturra <ian.zephir@gmail.com>
2017-01-07 18:01:21 -0300

    CRUD de productos

---
+++ models/Products.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "products".
+ *
+ * @property integer $id
+ * @property string $name
+ *
+ * @property SupermarketProduct[] $supermarketProducts
+ */
+class Products extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'products';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'string', 'max' => 256],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSupermarketProducts()
+    {
+        return $this->hasMany(SupermarketProduct::className(), ['product_id' => 'id']);
+    }
+}
