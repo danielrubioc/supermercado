b080051f80b20beb3f78875f77432b970add6e90
Daniel Rubio <drubio@admsys.cl>
2017-01-11 12:48:55 -0300

    crud employees

---
+++ models/Employees.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "employees".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $last_name
+ * @property integer $phone
+ * @property string $email
+ */
+class Employees extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'employees';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['phone'], 'integer'],
+            [['name', 'last_name', 'email'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'last_name' => 'Last Name',
+            'phone' => 'Phone',
+            'email' => 'Email',
+        ];
+    }
+}
